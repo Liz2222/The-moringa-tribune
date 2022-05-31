808c1429b55a3e4d245ce7f33de634c5f88d05e1
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-05-31 14:26:37 +0300

    added newsletter recipients model

--- news/views.py
+++ news/views.py
@@ -1,41 +1,61 @@
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+from django.core.exceptions import ObjectDoesNotExist
+from .forms import NewsLetterForm
 import datetime as dt
+from .models import Article
+
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
-
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    news = Article.todays_news()
 
-def convert_dates(dates):
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
 
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
 
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
+    return render(request, 'all-news/today-news.html',{'date': date,"news":news,"letterForm":form})
 
-def news_of_day(request):
-    date = dt.date.today()
 
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+
+def past_days_news(request,past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_today)
+    
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date":date,"news":news})
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
+
+def article(request,article_id):
+    try:
+        article = Article.objects.get(id = article_id)
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request,"all-news/article.html", {"article":article})
+    
