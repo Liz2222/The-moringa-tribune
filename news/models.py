3e0bb7566796b6887d44b3003e5dfb46e510e8fc
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-01 18:18:19 +0300

    Updated  moringa tribune

--- news/models.py
+++ news/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Editor(models.Model):
@@ -25,11 +27,11 @@ class tags(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length =60)
-    post = models.TextField()
-    editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
+    post = HTMLField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to = 'articles/',default='DEFAULT VALUE')
+    article_image = models.ImageField(upload_to = 'articles/')
 
     def __str__(self):
         return self.title
@@ -43,7 +45,7 @@ class Article(models.Model):
     def days_news(cls,date):
         news = cls.objects.filter(pub_date__date = date)
         return news
-
+    
     @classmethod
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
--- news/views.py
+++ news/views.py
@@ -1,27 +1,44 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse, Http404
-from django.core.exceptions import ObjectDoesNotExist
-from .forms import NewsLetterForm
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
-from .models import Article
+from .models import Article,NewsLetterRecipients
+from .forms import NewsLetterForm,NewArticleForm
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
+from .forms import NewArticleForm, NewsLetterForm
+from django.http import JsonResponse
+
+
 
 # Create your views here.
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
+    # if request.method == 'POST':
+    #     form = NewsLetterForm(request.POST)
+    #     if form.is_valid():
+    #         # print('valid')
+    #         name = form.cleaned_data['your_name']
+    #         email = form.cleaned_data['email']
+    #         recipient = NewsLetterRecipients(name = name,email =email)
+    #         recipient.save()
+    #         send_welcome_email(name,email)
 
-    if request.method == 'POST':
-        form = NewsLetterForm(request.POST)
-        if form.is_valid():
-            print('valid')
-    else:
-        form = NewsLetterForm()
 
-
-
+    #         HttpResponseRedirect('news_today')
+    # else:
+    form = NewsLetterForm()
     return render(request, 'all-news/today-news.html',{'date': date,"news":news,"letterForm":form})
 
+def newsletter(request):
+    name = request.POST.get('your_name')
+    email = request.POST.get('email')
 
+    recipient = NewsLetterRecipients(name=name, email=email)
+    recipient.save()
+    send_welcome_email(name, email)
+    data = {'success': 'You have been successfully added to mailing list'}
+    return JsonResponse(data)
 
 def past_days_news(request,past_date):
     
@@ -40,7 +57,7 @@ def past_days_news(request,past_date):
     return render(request, 'all-news/past-news.html', {"date":date,"news":news})
 
 def search_results(request):
-
+    
     if 'article' in request.GET and request.GET["article"]:
         search_term = request.GET.get("article")
         searched_articles = Article.search_by_title(search_term)
@@ -52,10 +69,24 @@ def search_results(request):
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html',{"message":message})
 
+@login_required(login_url='/accounts/login/')    
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
-    except ObjectDoesNotExist:
+    except DoesNotExist:
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
-    
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('newsToday')
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
